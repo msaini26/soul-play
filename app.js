070cf33b8c2ab5badeedc08546f77f350a625df9
Catherine Wang <c622wang@uwaterloo.ca>
2021-01-30 15:17:55 -0500

    testing different html render options

--- app.js
+++ app.js
@@ -24,6 +24,24 @@ app.get('/', function(req, res) {
 });
 app.listen(8080);
 
+/*
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.render('index');
+})
+
+app.listen(port, () => {
+    console.log('App listening on port' + port);
+
+});*/
+
 
 (async function () {
     try {
@@ -79,3 +97,10 @@ app.listen(8080);
     }
 
 })();
+
+const menuBtn = document.querySelector('.btn-menu');
+menuBtn.addEventListener('click', toggleMenu);
+
+function toggleMenu() {
+    console.log("this works");
+}
